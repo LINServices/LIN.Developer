3e14b99dd201026cf550dc1a9c9b39eafe2a21df
Alex <giraldojhong4@gmail.com>
2023-11-02 13:26:15 -0500

    Mejoras de usings.cs

--- LIN.Developer/Usings.cs
+++ LIN.Developer/Usings.cs
@@ -1,15 +1,23 @@
-﻿global using LIN.Developer;
-global using LIN.Developer.Services;
-global using LIN.Types.Developer.Enumerations;
-global using LIN.Types.Developer.Models;
-global using LIN.Types.Enumerations;
-global using LIN.Types.Responses;
+﻿// Framework.
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.IdentityModel.Tokens;
+
+// Local.
+global using LIN.Developer;
+global using LIN.Developer.Services;
+
+// Módulos.
 global using LIN.Modules;
-global using LIN.Developer.Data.Query;
+
+// Tipos.
+global using LIN.Types.Enumerations;
+global using LIN.Types.Responses;
 global using Http.ResponsesList;
 global using LIN.Types.Auth.Abstracts;
 global using LIN.Types.Auth.Models;
 global using LIN.Types.Auth.Enumerations;
+
+// Tipos locales.
+global using LIN.Types.Developer.Enumerations;
+global using LIN.Types.Developer.Models;
